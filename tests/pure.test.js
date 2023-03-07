baff0e3df9a2b32b8a6be932c700bd3e7c1a8324
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-03-07 22:58:10 +0200

    remove duplicated array

--- tests/pure.test.js
+++ tests/pure.test.js
@@ -1,33 +1,34 @@
 const {} = require("../js/pure.js");
 
+const arr = [
+  {
+    id: 1,
+    title: "title g g g g",
+    price: 49.5,
+    image: "https://image",
+    description: "lorem",
+    category: "cat1",
+  },
+  {
+    id: 2,
+    title: "title h h h h",
+    price: 74.9,
+    image: "http://dummyimage.com/714x600.png",
+    description: "lorem2",
+    category: "cat1",
+  },
+  {
+    id: 3,
+    title: "title j j j ",
+    price: 13.5,
+    image: "http://dummyimage.com/714x600.png",
+    description: "lorem3",
+    category: "cat2",
+  },
+];
+
 describe("eCommerce tests", () => {
   test("should remove item 2", () => {
-    const arr = [
-      {
-        id: 1,
-        title: "title g g g g",
-        price: 49.5,
-        image: "https://image",
-        description: "lorem",
-        category: "cat1",
-      },
-      {
-        id: 2,
-        title: "title h h h h",
-        price: 74.9,
-        image: "http://dummyimage.com/714x600.png",
-        description: "lorem2",
-        category: "cat1",
-      },
-      {
-        id: 3,
-        title: "title j j j ",
-        price: 13.5,
-        image: "http://dummyimage.com/714x600.png",
-        description: "lorem3",
-        category: "cat2",
-      },
-    ];
     const expected = removeItem(arr, 2);
     const actual = [
       {
@@ -52,32 +53,6 @@ describe("eCommerce tests", () => {
   });
 
   test("should add item 4", () => {
-    const arr = [
-      {
-        id: 1,
-        title: "title g g g g",
-        price: 49.5,
-        image: "https://image",
-        description: "lorem",
-        category: "cat1",
-      },
-      {
-        id: 2,
-        title: "title h h h h",
-        price: 74.9,
-        image: "http://dummyimage.com/714x600.png",
-        description: "lorem2",
-        category: "cat1",
-      },
-      {
-        id: 3,
-        title: "title j j j ",
-        price: 13.5,
-        image: "http://dummyimage.com/714x600.png",
-        description: "lorem3",
-        category: "cat2",
-      },
-    ];
     const expected = addProduct(arr, {
       id: 4,
       title: "title f f f ",
@@ -124,8 +99,16 @@ describe("eCommerce tests", () => {
     expect(expected).toEqual(actual);
   });
 
-    test("should edit item 4", () => {
-    const arr = [
+  test("should edit item 4", () => {
+    const expected = editProduct(arr, {
+      id: 3,
+      title: "title f f f ",
+      price: 13.5,
+      image: "http://dummyimage.com/714x600.png",
+      description: "lorem4",
+      category: "cat3",
+    });
+    const actual = [
       {
         id: 1,
         title: "title g g g g",
@@ -143,77 +126,17 @@ describe("eCommerce tests", () => {
         category: "cat1",
       },
       {
-        id: 3,
-        title: "title j j j ",
-        price: 13.5,
-        image: "http://dummyimage.com/714x600.png",
-        description: "lorem3",
-        category: "cat2",
-      },
-    ];
-    const expected = editProduct(arr, {
         id: 3,
         title: "title f f f ",
         price: 13.5,
         image: "http://dummyimage.com/714x600.png",
         description: "lorem4",
         category: "cat3",
-    });
-    const actual = [
-        {
-            id: 1,
-            title: "title g g g g",
-            price: 49.5,
-            image: "https://image",
-            description: "lorem",
-            category: "cat1",
-            },
-            {
-            id: 2,
-            title: "title h h h h",
-            price: 74.9,
-            image: "http://dummyimage.com/714x600.png",
-            description: "lorem2",
-            category: "cat1",
-            },
-            {
-            id: 3,
-            title: "title f f f ",
-            price: 13.5,
-            image: "http://dummyimage.com/714x600.png",
-            description: "lorem4",
-            category: "cat3",
-            },
+      },
     ];
-});
+  });
 
-test("should filter by category", () => {
-    const arr = [
-      {
-        id: 1,
-        title: "title g g g g",
-        price: 49.5,
-        image: "https://image",
-        description: "lorem",
-        category: "cat1",
-      },
-      {
-        id: 2,
-        title: "title h h h h",
-        price: 74.9,
-        image: "http://dummyimage.com/714x600.png",
-        description: "lorem2",
-        category: "cat1",
-      },
-      {
-        id: 3,
-        title: "title j j j ",
-        price: 13.5,
-        image: "http://dummyimage.com/714x600.png",
-        description: "lorem3",
-        category: "cat2",
-      },
-    ];
+  test("should filter by category", () => {
     const expected = filterByCategory(arr, "cat1");
     const actual = [
       {
@@ -237,8 +160,9 @@ test("should filter by category", () => {
     expect(expected).toEqual(actual);
   });
 
-    test("should filter by price", () => {
-    const arr = [
+  test("should filter by price", () => {
+    const expected = filterByPrice(arr, 50);
+    const actual = [
       {
         id: 1,
         title: "title g g g g",
@@ -255,65 +179,12 @@ test("should filter by category", () => {
         description: "lorem2",
         category: "cat1",
       },
-      {
-        id: 3,
-        title: "title j j j ",
-        price: 13.5,
-        image: "http://dummyimage.com/714x600.png",
-        description: "lorem3",
-        category: "cat2",
-      },
-    ];
-    const expected = filterByPrice(arr, 50);
-    const actual = [
-        {
-            id: 1,
-            title: "title g g g g",
-            price: 49.5,
-            image: "https://image",
-            description: "lorem",
-            category: "cat1",
-            },
-            {
-            id: 2,
-            title: "title h h h h",
-            price: 74.9,
-            image: "http://dummyimage.com/714x600.png",
-            description: "lorem2",
-            category: "cat1",
-            },
     ];
 
     expect(expected).toEqual(actual);
-});
+  });
 
-test("should filter by title", () => {
-    const arr = [
-      {
-        id: 1,
-        title: "title g g g g",
-        price: 49.5,
-        image: "https://image",
-        description: "lorem",
-        category: "cat1",
-      },
-      {
-        id: 2,
-        title: "title h h h h",
-        price: 74.9,
-        image: "http://dummyimage.com/714x600.png",
-        description: "lorem2",
-        category: "cat1",
-      },
-      {
-        id: 3,
-        title: "title j j j ",
-        price: 13.5,
-        image: "http://dummyimage.com/714x600.png",
-        description: "lorem3",
-        category: "cat2",
-      },
-    ];
+  test("should filter by title", () => {
     const expected = filterByTitle(arr, "title g");
     const actual = [
       {
@@ -329,8 +200,16 @@ test("should filter by title", () => {
     expect(expected).toEqual(actual);
   });
 
-test("should add product", () => {
-    const arr = [
+  test("should add product", () => {
+    const expected = addProduct(arr, {
+      id: 4,
+      title: "title f f f ",
+      price: 13.5,
+      image: "http://dummyimage.com/714x600.png",
+      description: "lorem4",
+      category: "cat3",
+    });
+    const actual = [
       {
         id: 1,
         title: "title g g g g",
@@ -355,51 +234,54 @@ test("should add product", () => {
         description: "lorem3",
         category: "cat2",
       },
-    ];
-    const expected = addProduct(arr, {
+      {
         id: 4,
         title: "title f f f ",
         price: 13.5,
         image: "http://dummyimage.com/714x600.png",
         description: "lorem4",
         category: "cat3",
-    });
-    const actual = [
-        {
-            id: 1,
-            title: "title g g g g",
-            price: 49.5,
-            image: "https://image",
-            description: "lorem",
-            category: "cat1",
-            },
-            {
-            id: 2,
-            title: "title h h h h",
-            price: 74.9,
-            image: "http://dummyimage.com/714x600.png",
-            description: "lorem2",
-            category: "cat1",
-            },
-            {
-            id: 3,
-            title: "title j j j ",
-            price: 13.5,
-            image: "http://dummyimage.com/714x600.png",
-            description: "lorem3",
-            category: "cat2",
-            },
-            {
-            id: 4,
-            title: "title f f f ",
-            price: 13.5,
-            image: "http://dummyimage.com/714x600.png",
-            description: "lorem4",
-            category: "cat3",
-            },
+      },
     ];
 
     expect(expected).toEqual(actual);
+  });
 
-});
+//     test("should get all products", () => {
+//     const expected = getAllProducts(arr);
+//     const actual = [
+//         {
+//             id: 1,
+//             title: "title g g g g",
+//             price: 49.5,
+//             image: "https://image",
+//             description: "lorem",
+//             category: "cat1",
+//         },
+//         {
+//             id: 2,
+//             title: "title h h h h",
+//             price: 74.9,
+//             image: "http://dummyimage.com/714x600.png",
+//             description: "lorem2",
+//             category: "cat1",
+//         },
+//         {
+//             id: 3,
+//             title: "title j j j ",
+//             price: 13.5,
+//             image: "http://dummyimage.com/714x600.png",
+//             description: "lorem3",
+//             category: "cat2",
+//         },
+//         {
+//             id: 4,
+//             title: "title f f f ",
+//             price: 13.5,
+//             image: "http://dummyimage.com/714x600.png",
+//             description: "lorem4",
+//             category: "cat3",
+//         },
+//     ];
+// });
 });
