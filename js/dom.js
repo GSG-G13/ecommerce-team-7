160f775bfe744ec37113a820702e6658ffaff124
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-03-08 16:24:36 +0200

    any thing

--- js/dom.js
+++ js/dom.js
@@ -2,9 +2,11 @@ const burger = document.querySelector(".burger");
 const navLinks = document.querySelector(".nav-links");
 const shop_btn = document.getElementById("shop-btn");
 
-shop_btn.addEventListener("click", () => {
-    document.getElementById('products').scrollIntoView();
-} );     
+if (shop_btn) {
+  shop_btn.addEventListener("click", () => {
+    document.getElementById("products").scrollIntoView();
+  });
+}
 
 burger.addEventListener("click", () => {
   navLinks.classList.toggle("nav-active");
--- js/pure.js
+++ js/pure.js
@@ -1,12 +1,22 @@
-const removeCartItem = (cart, id)=> {
-    const tempArr = [...cart];
-    let idx = tempArr.findIndex((e) => {
-        if (e.id == id)
-            return true;
-    });
-    if (idx == -1) return;
-    tempArr.splice(idx, 1);
-    return tempArr;
-}
+const removeCartItem = (cart, id) => {
+  const tempArr = [...cart];
+  let idx = tempArr.findIndex((e) => {
+    if (e.id == id) return true;
+  });
+  if (idx == -1) return;
+  tempArr.splice(idx, 1);
+  return tempArr;
+};
+// i know its duplicated , DRY principle is not applied here
+const deleteProduct = (product, id) => {
+  
+const tempProd = [...product];
+  let idx = tempProd.findIndex((e) => {
+    if (e.id == id) return true;
+  });
+  if (idx == -1) return ;
+  tempProd.splice(idx, 1);
+  return tempProd;
+};
 
-module.exports = { removeCartItem };
+module.exports = { removeCartItem, deleteProduct};
